4b6115ecf20db8e40aeab198fbd112af56d33f39
Kaochar <94604055+M-Khalekuzzaman@users.noreply.github.com>
2021-12-06 12:57:54 +0600

    Add files via upload

---
+++ Polymorphism.py
@@ -0,0 +1,15 @@
+# Built in polymorphic function
+print(len("Khalekuzzaman"))
+print(len([10,20,30]))
+
+
+# User defined polymorphic function
+def add(x,y, z =0) :
+    return x + y + z
+
+print("Sum is :",add(2,3))
+print("Sum is :",add(2,3,8))
+
+
+
+
